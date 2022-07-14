dfeabbfe69395db559f1f18bc81833146699b430
S-Adil <sana.adil.ak@gmail.com>
2022-07-14 13:43:20 -0400

    Update handler.js

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -90,7 +90,7 @@ module.exports.getAccessToken = async (event) => {
       return {
         statusCode: 200,
         headers: {
-          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Origin': '*'
         },
         body: JSON.stringify(token),
       };
@@ -100,6 +100,9 @@ module.exports.getAccessToken = async (event) => {
       console.error(err);
       return {
         statusCode: 500,
+        headers: {
+          'Access-Control-Allow-Origin': '*'
+        }
         body: JSON.stringify(err),
       };
     });
