bbc6857051f18595165f3daf59975c0ee0735448
S-Adil <sana.adil.ak@gmail.com>
2022-09-22 16:35:35 -0400

    added unit tests and code for 3 features of app

---
+++ src/__tests__/NumberofEvents.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import NumberOfEvents from '../NumberOfEvents';
+// import { mockData } from '../mock-data';
+
+describe('<NumberOfEvents /> component', () => {
+  let NumberOfEventsWrapper;
+  beforeAll(() => {
+    NumberOfEventsWrapper = shallow(<NumberOfEvents />);
+  });
+
+  test('render text input', () => {
+    expect(NumberOfEventsWrapper.find('.number')).toHaveLength(1);
+  });
+
+  test('renders text input correctly', () => {
+    const numberOfEvents = NumberOfEventsWrapper.state('numberOfEvents');
+    expect(NumberOfEventsWrapper.find('.number').prop('value')).toBe(numberOfEvents);
+  });
+
+
+
+});
